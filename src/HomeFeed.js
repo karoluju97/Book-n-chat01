97c5a1e1f85923b426e6a1d3aaef91b5ee534a4b
Karol Uju Amajirionwu <a.karoluju97@gmail.com>
2021-03-04 04:34:46 +0000

    Setting up firebase with the post function

--- src/HomeFeed.js
+++ src/HomeFeed.js
@@ -1,19 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import "./HomeFeed.css"
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption'
 import ImageIcon from '@material-ui/icons/Image';
 import CheckIcon from '@material-ui/icons/Check';
 import Post from './Post'
+import { db } from './firebase'
+import firebase from './firebase'
 
 
 function HomeFeed() {
 
+    const [input, setInput] = useState('');
     const [posts, setPosts] = useState([]);
+    
+    useEffect(() => {
+        db.collection("posts").onSnapshot(snapshot => ( setPosts(snapshot.docs.map(doc => ({
+            id: doc.id,
+            data: doc.data(),
+        }        ))))
+    }, []);
 
     const sendPost = e => {
         e.preventDefault();
-    }
+
+        db.collection('posts').add({
+            name: 'sonny',
+            description: 'test',
+            message: input,
+            photoUrl:'',
+            timestamp: firebase.firestore.Fieldvalue.serverTimestamp()
+        }),
+    };
 
     return (
         <div className='homeFeed'>
@@ -21,7 +39,7 @@ function HomeFeed() {
                 <div className='homeFeed_input'>
                     <CreateIcon />
                     <form>
-                        <input type="text"/>
+                        <input value={input} onChange={e => setInput(e.target.value)} type="text"/>
                         <button onClick={sendPost} type="submit">Post</button>
                     </form>
                 </div>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+l
+const firebaseConfig = {
+    apiKey: "AIzaSyAXWj5M9W_2AdCgOaGChBB00HZMq7dWjTs",
+    authDomain: "book-n-chat.firebaseapp.com",
+    projectId: "book-n-chat",
+    storageBucket: "book-n-chat.appspot.com",
+    messagingSenderId: "320577125176",
+    appId: "1:320577125176:web:c696df768185bb774e9fcd",
+    measurementId: "G-MNM0PDDJ4S"
+  };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
