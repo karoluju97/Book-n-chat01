2acb0d1fc6502492568185ace67ebf09842a46ae
Karol Uju Amajirionwu <a.karoluju97@gmail.com>
2021-03-04 03:44:32 +0000

    designing the post buttons

--- src/HomeFeed.js
+++ src/HomeFeed.js
@@ -3,6 +3,9 @@ import "./HomeFeed.css"
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption'
 import ImageIcon from '@material-ui/icons/Image';
+import CheckIcon from '@material-ui/icons/Check';
+import Post from './Post'
+
 
 function HomeFeed() {
     return (
@@ -17,9 +20,13 @@ function HomeFeed() {
                 </div>
                 <div className="homeFeed_inputOption">
                     <InputOption Icon={ImageIcon} title='Upload Image'/>
-
+                    <InputOption Icon={CheckIcon} title='Promotion'/>
                 </div>
             </div>
+
+            {/* Posts */}
+            <Post name='karol' description='this is a test' message='hello'/>
+            
         </div>
     )
 }
---
+++ src/Post.js
@@ -0,0 +1,36 @@
+import { Avatar } from "@material-ui/core"
+import React from 'react'
+import './Post.css'
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import InputOption from './InputOption'
+import CommentIcon from '@material-ui/icons/Comment';
+import ShareIcon from '@material-ui/icons/Share';
+import BookIcon from '@material-ui/icons/Book';
+
+function Post({ name, description, message, photoUrl }) {
+    return (
+        <div className='Post'>
+            <div className="post_header">
+                <Avatar />
+                <div className="post_info">
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+
+            <div className="past_body">
+                <p>{message}</p>
+            </div> 
+
+            <div className="post_buttons">
+                <InputOption Icon={ThumbUpIcon} title="Like" />    
+                <InputOption Icon={CommentIcon} title="Comment" />    
+                <InputOption Icon={ShareIcon} title="Share" />    
+                <InputOption Icon={BookIcon} title="Bookmark" />    
+
+            </div>           
+        </div>
+    )
+}
+
+export default Post
