be19ee34c183242ff1e121cb6d5fb31053d4ec88
Karol Uju Amamjirionwu <a.karoluju97@gmail.com>
2021-04-21 14:33:12 +0100

    Finished with the sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,9 +2,23 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+
+    const recentSearches = (topic) => (
+        <div className="sidebar_recentSearches">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+
+    )
+
     return(
         <div className="sidebar">
-            <p>Recent Searchs</p>
+            <p>Recent Searches</p>
+            {recentSearches("Harry Potter and the Goblet of Fire")}
+            {recentSearches("Shingeki no Kyojin")} 
+            {recentSearches("I am Zlatan Ibrahimovic")} 
+            {recentSearches("Marvel Comics")}
+            {recentSearches("DC Comics")}
         </div> 
         );
 }
